367b9cd1a381735dafbd82475f552a9654615e2e
Christine Withers <withers@ebi.ac.uk>
2025-10-03 11:21:00 +0100

    Add files via upload

--- data_processing/triple_formatting.py
+++ data_processing/triple_formatting.py
@@ -3,16 +3,9 @@ from typing import List, Optional
 # Aim - Format files in consistent triple format
 
 def formatting_triples(infile_path: str,
-                       sep: str,
-                       subject: str,
-                       subject_meta: Optional[List] = None,
-                       predicate: str,
-                       predicate_meta: Optional[List] = None,
-                       object: str,
-                       object_meta: Optional[List] = None):
+                       sep: str):
     """
     Here we are expecting a tsv/csv as input
-    - Declare subject, predicate, object of input file
     - Write separate node / relationship lists to file
     - Optional metadata additions
     """
@@ -20,11 +13,24 @@ def formatting_triples(infile_path: str,
     print(input_df.columns)
     print(input_df)
 
-formatting_triples(infile_path="/Users/withers/GitProjects/aws_hackathon/ot_drug_indication.tsv",
-                   sep="\t",
-                   subject="drug_id",
-                   subject_meta = []
-                   predicate = " ",
-                   predicate_meta = []
-                   object = "disease_id"
-                   object_meta = [])
+    # Prepare drugs nodes (unique)
+    drugs_df = input_df[['drug_id', 'drug_name', 'isApproved', 'hasBeenWithdrawn']].drop_duplicates()
+    drugs_df.rename(columns={'drug_id': 'id', 'drug_name': 'name'}, inplace=True)
+    
+    # Prepare disease nodes (unique)
+    diseases_df = input_df[['disease_id', 'disease']].drop_duplicates()
+    diseases_df.rename(columns={'disease_id': 'id', 'disease': 'name'}, inplace=True)
+
+    # Prepare relationships with features
+    relationships_df = input_df[['drug_id', 'disease_id', 'maximumClinicalTrialPhase']].copy()
+    relationships_df.rename(columns={'drug_id': 'subject',
+                                     'disease_id': 'object',
+                                     'maximumClinicalTrialPhase': 'maxClinicalTrialPhase'}, inplace=True)
+
+    base_path = "/Users/withers/GitProjects/aws_hackathon/Variant_Drug_KG/data_processing/OTs_drugdis_data"
+    # Write CSVs for bulk import into Neo4j
+    drugs_df.to_csv(f"{base_path}/drugs.csv", index=False)
+    diseases_df.to_csv(f"{base_path}/diseases.csv", index=False)
+    relationships_df.to_csv(f"{base_path}/drug_disease_interactions.csv", index=False)
+
+formatting_triples(infile_path="/Users/withers/GitProjects/aws_hackathon/ot_drug_indication.tsv", sep="\t")
