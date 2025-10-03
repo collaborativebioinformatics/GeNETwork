bc2de44e4c53ace25f346c60ed1eab63d4a97db9
Christine Ann Withers <withers@ebi.ac.uk>
2025-10-03 12:48:09 +0100

    dir cleanup

--- data_processing/OTs_drug_disease/ot_drug_data.py
+++ data_processing/OTs_drug_disease/ot_drug_data.py
@@ -1,36 +1,89 @@
 import pandas as pd
-from typing import List, Optional
-# Aim - Format files in consistent triple format
 
-def formatting_triples(infile_path: str,
-                       sep: str):
-    """
-    Here we are expecting a tsv/csv as input
-    - Write separate node / relationship lists to file
-    - Optional metadata additions
-    """
-    input_df = pd.read_csv(infile_path, sep=sep)
-    print(input_df.columns)
-    print(input_df)
+def read_infile(file_path: str) -> pd.DataFrame:
+    return pd.read_parquet(file_path)
 
-    # Prepare drugs nodes (unique)
-    drugs_df = input_df[['drug_id', 'drug_name', 'isApproved', 'hasBeenWithdrawn']].drop_duplicates()
-    drugs_df.rename(columns={'drug_id': 'id', 'drug_name': 'name'}, inplace=True)
+def parse_drug_ind(input_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Triple format would be:
+        DRUG -- TREATS --> DISEASE
+        - id                indications
+                                - for each indication
+                                - efoName
+                                - disease
+                                - maxphase
+    Return pd.DataFrame
+    """
+    results = []
+    for i, row in input_df.iterrows():
+        indications = row["indications"]
+        for ind in indications:
+            results.append({
+                    "drug_id" : row["id"],
+                    "disease": ind["efoName"],
+                    "disease_id": ind["disease"],
+                    }
+            ) # TODO - Only other part to extract here would be reference data?
+
+    return pd.DataFrame(results)
+
+def try_catch(row, element):
+    try:
+        return row[element]["rows"]
+    except:
+        return []
+
+def parse_drug_mol(input_df: pd.DataFrame) -> pd.DataFrame:
+    results = []
+    for i, row in input_df.iterrows():
+        
+        results.append({
+                "drug_name" : row["name"],
+                "drug_id": row["id"],
+                "isApproved": row["isApproved"],
+                "hasBeenWithdrawn": row["hasBeenWithdrawn"],
+                "maximumClinicalTrialPhase": row["maximumClinicalTrialPhase"]
+                }
+        ) # TODO - Is this all the data we wish to extract?
+
+    return pd.DataFrame(results)
+
+def join_dataframes(df1: pd.DataFrame,
+                    df2: pd.DataFrame,
+                    merge_on_l: str,
+                    merge_on_r: str) -> pd.DataFrame:
+
+    merged_df = pd.merge(df1, df2, left_on = merge_on_l, right_on = merge_on_r, how="left", suffixes=('_left', '_right'))
+    print(merged_df.columns)
+    # Merge and clean up
+    merged_df['drug_id'] = merged_df['drug_id'].fillna(merged_df['drug_id'])
+    # merged_df = merged_df.drop(['drug_left', 'drug_right'], axis=1)
+    merged_df = merged_df.drop_duplicates()
+    merged_df = merged_df[["drug_id", "drug_name", "disease_id", "disease", "maximumClinicalTrialPhase", "isApproved", "hasBeenWithdrawn"]]
+
+    return merged_df
+
+if __name__ == "__main__":
+    """
+    (1.) Read-in download of OTs drug indication data
+         - OTs data accessed at ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/25.09/output/drug_indication
+    (2.) Read-in download of OTs drug molecule data
+         - OTs data accessed at ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/25.09/output/drug_molecule
+    (3.) Desired output: triple format suitable for KG ingestion
+    """
+
+    drugind_path = "drug_indication/part-00000-0e2ed002-bc1a-4c53-855e-822517b3bdf3-c000.snappy.parquet"
+    drug_indication = read_infile(file_path = drugind_path)
+    drug_indication_df = parse_drug_ind(input_df = drug_indication)
+    print(drug_indication_df.columns)
     
-    # Prepare disease nodes (unique)
-    diseases_df = input_df[['disease_id', 'disease']].drop_duplicates()
-    diseases_df.rename(columns={'disease_id': 'id', 'disease': 'name'}, inplace=True)
+    drugmol_path = "drug_molecule/part-00000-35323264-2241-4256-b643-3a92cd0230e5-c000.snappy.parquet"
+    drug_molecule = read_infile(file_path = drugmol_path)
+    drug_molecule_df = parse_drug_mol(input_df = drug_molecule)
+    print(drug_molecule_df.columns)
 
-    # Prepare relationships with features
-    relationships_df = input_df[['drug_id', 'disease_id', 'maximumClinicalTrialPhase']].copy()
-    relationships_df.rename(columns={'drug_id': 'subject',
-                                     'disease_id': 'object',
-                                     'maximumClinicalTrialPhase': 'maxClinicalTrialPhase'}, inplace=True)
-
-    base_path = "/Users/withers/GitProjects/aws_hackathon/Variant_Drug_KG/data_processing/OTs_drugdis_data"
-    # Write CSVs for bulk import into Neo4j
-    drugs_df.to_csv(f"{base_path}/drugs.csv", index=False)
-    diseases_df.to_csv(f"{base_path}/diseases.csv", index=False)
-    relationships_df.to_csv(f"{base_path}/drug_disease_interactions.csv", index=False)
-
-formatting_triples(infile_path="/Users/withers/GitProjects/aws_hackathon/ot_drug_indication.tsv", sep="\t")
+    # Here, tie together the two dataframes, finalising triples of DRUG -- TARGETS -- DISEASE, with
+    # associated metadata
+    merged_df = join_dataframes(drug_indication_df, drug_molecule_df, merge_on_l= "drug_id", merge_on_r="drug_id")
+    print(merged_df)
+    merged_df.to_csv("./ot_drug_indication.tsv", sep="\t")
